0fcbb087921cc506bb49ae1fec08cd2cddf72dd1
hwang381 <wanghanjie2012@gmail.com>
2019-09-21 21:02:31 +0800

    polling seems to be more reiable

---
+++ extension/monitor.js
@@ -0,0 +1,31 @@
+let lastUrl;
+
+setInterval(async () => {
+    const activeTabs = await browser.tabs.query({
+        currentWindow: true,
+        active: true
+    });
+    if (activeTabs.length !== 1) {
+        console.error("there is not one active tab??");
+        return
+    }
+    const activeTab = activeTabs[0];
+
+    const url = activeTab.url;
+    console.log(url);
+    if (url.startsWith("about")) {
+        return
+    }
+    if (url === lastUrl) {
+        return
+    }
+    try {
+        await fetch('http://127.0.0.1:16789/api/event/browser_tab_focus', {
+            method: 'post',
+            body: url
+        })
+    } catch (error) {
+        console.error(error)
+    }
+    lastUrl = url
+}, 1000);
