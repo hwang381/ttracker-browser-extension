4f8e6c1544e0e7ff151bd6b94c12195718a932fc
hwang381 <wanghanjie2012@gmail.com>
2019-09-25 19:01:07 +0800

    specify gen for url pings

--- extension/monitor.js
+++ extension/monitor.js
@@ -1,3 +1,6 @@
+let gen = 0;
+let firstNoFocus = true;
+
 setInterval(async () => {
     const windows = await browser.windows.getAll({
         populate: true
@@ -5,8 +8,14 @@ setInterval(async () => {
     const focusedWindows = windows.filter(w => w.focused);
     if (focusedWindows.length === 0) {
         console.log("no window is focused, ignore");
+        if (firstNoFocus) {
+            console.log("first no focus, switching gen for next batch of pings");
+            gen = gen === 0 ? 1 : 0;
+            firstNoFocus = false
+        }
         return
     }
+    firstNoFocus = false;
     if (focusedWindows.length > 1) {
         console.log("more than one window is focused (why?), ignore");
         return
@@ -23,10 +32,18 @@ setInterval(async () => {
     }
     const activeTab = activeTabs[0];
     const url = activeTab.url;
+    if (url.startsWith("about")) {
+        console.log("an url starts with about, ignore");
+        return
+    }
     try {
         await fetch('http://localhost:16789/api/ping/browser', {
             method: 'post',
-            body: url
+            body: JSON.stringify({
+                // TODO: parse out only the host for better privacy
+                url: url,
+                gen: gen
+            })
         }).then(() => {
             console.log(`pinged ${url}`)
         })
